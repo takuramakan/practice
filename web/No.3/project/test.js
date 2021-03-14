87cbb1818934e337e6ba438ce8efcf23c1a3bd90
takuramakan <takuramakan5364@gmail.com>
2021-03-14 16:35:28 +0900

    勉強会 ハンバーガーメニュー実装

--- web/No.3/project/test.js
+++ web/No.3/project/test.js
@@ -25,3 +25,53 @@ document.addEventListener("DOMContentLoaded", function(){
         var name = $('li[name="notAnimeYerrow"]').attr('name', 'doAnimeYerrow');;
     });
 });
+$(function() {
+    //マウスダウンの位置
+    var pos1;
+    var pos2;
+    //要素位置の修正値
+    var posX1;
+    var posY1;
+ 
+    //要素内でマウスがあった場合
+    $("#hanbaga").mouseover(function(evt1) {
+  
+            //要素の位置取得
+            pos1 = $("#hanbaga").position();
+            //要素位置を取得して修正値を計算
+            posX1 = evt1.clientX - pos1.left;
+            posY1 = evt1.clientY - pos1.top;
+   
+    });
+
+    //ドキュメント上でマウスポインタが動いた場合
+    $("#hanbagaContainer").mousemove(function(evt2) {
+        //ドラッグ中の場合
+            //要素位置をCSSで設定
+            $("#hanbaga").css("left",(evt2.clientX - posX1));
+            $("#hanbaga").css("top",(evt2.clientY - posY1));
+    });
+/*
+    //ドキュメント上でマウスポインタが離れた場合
+    $("#hanbagaContainer").mouseout(function() {
+        //ドラッグ中の場合
+            //要素位置をCSSで設定
+            var leftper = "20%";
+            var rightper = "60%";
+            $("#hanbaga").css("left",leftper);
+            $("#hanbaga").css("top",rightper);
+    });
+*/
+    var clickFlg = false;
+    $("#hanbaga").click(function() {
+        var leftper = "0%";
+        if (clickFlg) {
+            leftper = "100%";
+            clickFlg = false;
+        } else {
+            leftper = "70%";
+            clickFlg = true;
+        }
+        $("#hanbagamenuContainer").css("left",leftper);
+    })
+});

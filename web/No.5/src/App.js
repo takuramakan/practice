2f8d8a5aadcb265190e5328e89a05af83945997a
takuramakan <takuramakan5364@gmail.com>
2021-05-05 07:02:00 +0900

    props

--- web/No.5/src/App.js
+++ web/No.5/src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 
 function App() {
-  //const greeting = "Hi!";
-  //const dom = <h1 className="foo">{greeting}</h1>;
+  const profiles = [
+    { name: "Taro", age:10},
+    { name: "Hanako", age:5},
+    { name: "NoName"}
+  ]
   return (
-    //このHTMLのような記載がJSX
-    //<h1>Hello,world!!</h1>
-    //dom
-    //Reactの戻り値は一つのタグでないといけないreact.fragmentはそのためのタグ。HTML上には現れない
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => {console.log("I am clicked!")}}></input>
-    </React.Fragment>
-  );
+    <div>
+      {
+        profiles.map((profile,index) => {
+          return <User name={profile.name} age={profile.age}/>
+        })
+      }
+    </div>
+    );
 }
 
-//import React from "react";
+const User = (props) => {
+  return <div>Hi, I am {props.name}! and {props.age} years old!</div>
+}
 
-//Jsで書き換えるとこんな感じ
-// function App() {
-//   return (
-//     React.createElement(
-//       "div",
-//       null,
-//       "Hello,world!!"
-//     )
-//   );
-// }
-
+User.defaultProps = {
+  age: 1
+}
 export default App;

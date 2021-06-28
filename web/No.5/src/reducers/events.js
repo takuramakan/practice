e55896663a9f0ca2cc19577a4a98e6296b2240c3
takuramakan <takuramakan5364@gmail.com>
2021-06-28 22:52:33 +0900

    画面表示完了

--- web/No.5/src/reducers/events.js
+++ web/No.5/src/reducers/events.js
@@ -1,11 +1,14 @@
 //countの状態を保持するreducer
-
+import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
 export default (state = {}, action) => {
     switch(action.type) {
         case READ_EVENTS:
-            return state
+            /*action（データ取得結果）をidをキーにしたマップ状のデータで返却する  ↓こんな感じ
+            1: {id: 1, token: "token123", title: "Let's have an event 1!", body: "This is the body for event 1.", created_at: "2021-06-22T12:04:22.747Z", …}
+            */
+            return _.mapKeys(action.response.data,'id')
         default:
             return state
     }

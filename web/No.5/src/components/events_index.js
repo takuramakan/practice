2f53d3917350fa0f23f5b64b32a384ab2d05ad9e
takuramakan <takuramakan5364@gmail.com>
2021-06-28 23:17:04 +0900

    画面遷移実装

--- web/No.5/src/components/events_index.js
+++ web/No.5/src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import _ from 'lodash'
+import {Link} from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -35,6 +36,8 @@ class EventsIndex extends Component {
             {this.renderEvents()}
           </tbody>
         </table>
+
+        <Link to="/events/new">New Events</Link>
       </React.Fragment>
     )
   }

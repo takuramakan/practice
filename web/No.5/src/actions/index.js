6cced5d357f2cc3fa4d8ffb1f2377f01f37d3227
takuramakan <takuramakan5364@gmail.com>
2021-09-23 14:12:59 +0900

    送信イベント完成

--- web/No.5/src/actions/index.js
+++ web/No.5/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 //reducerでも使うため、定数定義
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -12,3 +13,8 @@ export const readEvents = () => async dispatch =>{
     dispatch({type: READ_EVENTS, response})
 }
 
+//ActionCreater:アクションを返す関数
+export const postEvent = values => async dispatch =>{
+    const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`,values)
+    dispatch({type: CREATE_EVENT, response})
+}
--- web/No.5/src/reducers/index.js
+++ web/No.5/src/reducers/index.js
@@ -1,8 +1,8 @@
 //各reducerを結合するためのreducer
-
 import { combineReducers } from 'redux'　//reactが用意している各reducerを結合する関数
+import { reducer as form} from 'redux-form'
 import events from './events'
 
-export default combineReducers ({events})
+export default combineReducers ({events,form})
 //↓複数のreducerがある場合
 //export default combineReducers ({foo, bar, baz})

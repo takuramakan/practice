33ca61254aec5bd670d15aa724da12c036bf3dfa
takuramakan <takuramakan5364@gmail.com>
2022-02-20 19:16:33 +0900

    bootstrap適用

--- web/No.7_React/source/script.js
+++ web/No.7_React/source/script.js
@@ -2,7 +2,27 @@ let dom = document.querySelector('#root');
 
 //Reactの仮想DOMにエレメント生成
 let element = React.createElement(
-    'p', {}, "Hello React"
+    'div', {}, [
+        React.createElement(
+            'h2', {}, "Hello!"
+        ),
+        React.createElement(
+            'h3', {}, "React sample page."
+        ),
+        React.createElement(
+            'ul', {}, [
+                React.createElement(
+                    'li', {}, "First item."
+                ),
+                React.createElement(
+                    'li', {}, "Second item."
+                ),
+                React.createElement(
+                    'li', {}, "Third item."
+                )                                
+            ]
+        )
+    ]
 )
 
 //画面への反映

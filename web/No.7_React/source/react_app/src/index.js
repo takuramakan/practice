b9f77bc2096651dd0224812f10bb2bd2c8392412
takuramakan <takuramakan5364@gmail.com>
2022-03-26 19:31:36 +0900

    コンポーネントで属性を使用する

--- web/No.7_React/source/react_app/src/index.js
+++ web/No.7_React/source/react_app/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="App"
+      message="This is App Component"/>
   </React.StrictMode>,
   document.getElementById('root')
 );

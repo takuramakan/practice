4a970db74f107615df0d803db32ffd741a9f99f0
takuramakan <takuramakan5364@gmail.com>
2022-03-27 19:46:33 +0900

    formを使ってみる

--- web/No.7_React/source/react_app/src/App.js
+++ web/No.7_React/source/react_app/src/App.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
-import Rect from './Rect'
 
 class App extends Component {
+  input = ''
+
   constructor(props){
     super(props)
     this.state = {
-      msg:'Hello',
-      count:0
+      title: 'input form',
+      message: 'type your name'
     }
-    let timer = setInterval(()=>{
-      this.setState({
-        count:this.state.count + 1,
-        msg:'[ count: ' + this.state.count + ']'
-      })
-    },1000)
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
+  }
+
+  doChange(event) {
+    this.input = event.target.value;
+  }
+
+  doSubmit(event) {
+    this.setState({
+      title: 'send form',
+      message: 'Hello, ' + this.input + '!!'
+    })
+    event.preventDefault()
   }
 
   render(){
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle</p>
-        <p className='alert alert-warning'>{this.state.msg}</p>
-        <p className='alert alert-dark'>{this.props.msg}</p>
+        <h4>{this.state.title}</h4>
+        <p className='card h5 p-3'>{this.state.message}</p>
+        <div className='alert alert-primary mt-3'>
+          <form onSubmit={this.doSubmit}>
+            <div className='form-group'>
+              <label>Message:</label>
+              <input type='text' className='form-control' onChange={this.doChange} />
+            </div>
+            <input type='submit' className='btn btn-primary' value='Click' />
+          </form>
+        </div>
       </div>
     </div>
   }
--- web/No.7_React/source/react_app/src/index.js
+++ web/No.7_React/source/react_app/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App msg='Hello App'/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );

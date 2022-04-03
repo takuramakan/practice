5a86cf40d2ac04a8b1f7956a1938eb5285cf7522
takuramakan <takuramakan5364@gmail.com>
2022-04-03 14:55:37 +0900

    フックでステートを表示する

--- web/No.7_React/source/react_app/src/App.js
+++ web/No.7_React/source/react_app/src/App.js
@@ -1,59 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  input = ''
+function App() {
+  const [message] = useState("welcome to Hooks!!")
 
-  constructor(props){
-    super(props)
-    this.state = {
-      title: 'input form',
-      message: 'type your name',
-      max: 10
-    }
-    this.doCheck = this.doCheck.bind(this)
-  }
-
-  doCheck(event) {
-    alert(event.target.value + "は長すぎます（最大" + this.state.max + "文字）")
-  }
-
-  render(){
-    return <div>
-      <h1 className="bg-primary text-white display-4">React</h1>
+  return (
+    <div>
+      <h1 className="bg-primary text-white display-4 ">React</h1>
       <div className="container">
-        <h4>{this.state.title}</h4>
-        <Message maxlength={this.state.max} onCheck={this.doCheck} />
+        <h4 className="my-3">Hooks sample</h4>
+        <div className="alert alert-primary text-center">
+          <p className='h5'>{message}</p>
+        </div>
       </div>
     </div>
-  }
-}
-
-class Message extends Component {
-  li = {
-    fontSize:'14pt',
-    fontWeight: 'bold',
-    color: '#090'
-  }
-
-  constructor(props) {
-    super(props)
-    this.doChange = this.doChange.bind(this)
-  }
-
-  doChange(e) {
-    if (e.target.value.length > this.props.maxlength) {
-      this.props.onCheck(e)
-      e.target.value = e.target.value.substr(0,this.props.maxlength)
-    }
-  }
-
-  render() {
-    return <div className='form-group'>
-      <label>input:</label>
-      <input type='text' className='form-control' onChange={this.doChange} />
-    </div>
-  }
+  )
 }
 
 export default App;

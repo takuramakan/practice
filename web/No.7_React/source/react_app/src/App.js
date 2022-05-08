148a6d7aa830a708a960cfc6d7002bb3d00fd408
takuramakan <takuramakan5364@gmail.com>
2022-05-08 11:23:11 +0900

    数字をカウントする独自フック

--- web/No.7_React/source/react_app/src/App.js
+++ web/No.7_React/source/react_app/src/App.js
@@ -1,25 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const clickFunc = () => {
-    setCount(count + 1)
+function useCounter() {
+  const [num, setNum] = useState(0)
+
+  const count = () => {
+    setNum(num + 1)
   }
 
+  return [num, count]
+}
+
+function AlertMessage(props){
+  const [counter, plus] = useCounter()
+
+  return (
+    <div className="alert alert-primary h5 text-center">
+      <h4>count: {counter}</h4>
+      <button onClick={plus} className="btn btn-primary">count</button>
+    </div>
+  )
+}
+
+function App() {
   return (
     <div>
-      <h1 className="bg-primary text-white display-4 ">React</h1>
-      <div className="container">
-        <h4 className="my-3">Hooks sample</h4>
-        <div className="alert alert-primary text-center">
-          <p className='h5 mb-3'>click: {count} times</p>
-          <div>
-            <button className='btn btn-primary' onClick={clickFunc}>
-              click me
-            </button>
-          </div>
-        </div>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='container'>
+        <h4 className='my-3'>Hooks Sample</h4>
+        <AlertMessage/>
       </div>
     </div>
   )

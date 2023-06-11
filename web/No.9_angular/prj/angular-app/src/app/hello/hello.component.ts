b7752eaea2619a079aa48b91972648f239fa347d
takuramakan <takuramakan5364@gmail.com>
2023-06-11 13:29:51 +0900

    chapter3_formGroup

--- web/No.9_angular/prj/angular-app/src/app/hello/hello.component.ts
+++ web/No.9_angular/prj/angular-app/src/app/hello/hello.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl , FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -10,22 +10,31 @@ export class HelloComponent implements OnInit {
   title:string;
   message:string;
   text1:string;
-  myControl:FormControl;
+  myControl:FormGroup;
 
   constructor() {
     this.title = "";
     this.message = "";
     this.text1 = "OK";
-    this.myControl = new FormControl('OK');
+    this.myControl = new FormGroup({
+      name: new FormControl(''),
+      mail: new FormControl(''),
+      age: new FormControl(0)
+    });
   }
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'ngModelを使う';
+    this.message = 'formControlを使う';
     this.text1 = 'OK';
   }
 
   doClick(){
     this.message = '「'+ this.myControl.value + '」と書きましたね';
+  }
+
+  onSubmit(){
+    let result = this.myControl.value;
+    this.message = JSON.stringify(result);
   }
 }

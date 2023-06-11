643047dd329b0290ab956ff0c5c8586c04b55455
takuramakan <takuramakan5364@gmail.com>
2023-06-11 12:42:06 +0900

    chapter3_input,button

--- web/No.9_angular/prj/angular-app/src/app/hello/hello.component.ts
+++ web/No.9_angular/prj/angular-app/src/app/hello/hello.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-hello',
@@ -8,51 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class HelloComponent implements OnInit {
   title:string;
   message:string;
-  price:number;
-  now:Date;
-  styleClass:string;
-  count:number;
-  input:string;
-  visible:boolean;
-  data:string[];
+  text1:string;
+  myControl:FormControl;
 
   constructor() {
     this.title = "";
     this.message = "";
-    this.price = 0;
-    this.now = new Date();
-    this.styleClass = "red";
-    this.count = 0;
-    this.input = "";
-    this.visible = true;
-    this.data = ['1','2','3'];
-
-    setInterval(
-      ()=>{
-        this.now = new Date();
-        this.styleClass = this.styleClass == 'red' ? '' : 'red';
-      },1000);
+    this.text1 = "OK";
+    this.myControl = new FormControl('OK');
   }
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'this is my first component';
-    this.price = 500;
+    this.message = 'ngModelを使う';
+    this.text1 = 'OK';
   }
 
-  today() {
-    return new Date().toLocaleString();
-  }
-
-  doClick() {
-    /*
-    this.message = ++this.count + "回クリックしました";
-     */
-    this.visible = !this.visible;
-  }
-
-  doType(val:string) {
-    this.input = val;
-    this.message = 'you type:' + this.input;
+  doClick(){
+    this.message = '「'+ this.myControl.value + '」と書きましたね';
   }
 }
